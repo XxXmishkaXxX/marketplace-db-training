199600157e0c23a73f965c55d70eb56914beee33
Misha Kutsov <misha@misha.ru>
2025-08-28 02:40:35 +0500

    add users table

---
+++ src/db/models/accounts/users.py
@@ -0,0 +1,35 @@
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from db.base import Base
+from .users_roles import users_roles
+
+#types
+from typing import List
+from datetime import datetime
+from sqlalchemy import Boolean, Date
+from sqlalchemy_utils import PasswordType, EmailType, PhoneNumberType
+from models.types import uuid_pk, str256_not_null
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id: Mapped[uuid_pk]
+    name: Mapped[str256_not_null]
+    email: Mapped[str] = mapped_column(EmailType, nullable=False, index=True)
+    phone_number: Mapped[str] = mapped_column(PhoneNumberType(region="RU"), nullable=False, index=True)
+    password_hash: Mapped[str] = mapped_column(
+        PasswordType(schemes=['bcrypt'], deprecated='auto'),
+        nullable=False
+    )
+    date_of_birth: Mapped[Date] = mapped_column(Date, nullable=True)
+    created_at: Mapped[datetime] = mapped_column(Date, default=datetime.utcnow)
+    updated_at: Mapped[datetime] = mapped_column(Date, default=datetime.utcnow, onupdate=datetime.utcnow)
+
+    is_active: Mapped[bool] = mapped_column(Boolean, default=True, nullable=False)
+    is_verified: Mapped[bool] = mapped_column(Boolean, default=False, nullable=False)
+    
+    roles: Mapped[List["Role"]] = relationship(
+        "Role",
+        secondary=users_roles,
+        back_populates="users"
+    )
