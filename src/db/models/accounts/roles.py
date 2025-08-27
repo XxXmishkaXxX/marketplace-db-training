1ca14c78d5a9caa5b84cadab833d4804c6c92043
Misha Kutsov <misha@misha.ru>
2025-08-28 02:41:14 +0500

    add roles table

---
+++ src/db/models/accounts/roles.py
@@ -0,0 +1,19 @@
+from typing import List
+from db.base import Base
+from sqlalchemy.orm import Mapped, relationship
+from models.types import uuid_pk, str256_not_null, str500
+from .users_roles import users_roles
+
+class Role(Base):
+    __tablename__ = "roles"
+
+    id: Mapped[uuid_pk]
+    name: Mapped[str256_not_null]
+    description: Mapped[str500]
+
+    users: Mapped[List["User"]] = relationship(
+        "User",
+        secondary=users_roles,
+        back_populates="roles"
+    )
+     
