77dfe0136a5a001e63000b743227b84069613468
Misha Kutsov <misha@misha.ru>
2025-08-31 00:36:11 +0500

    add order itmes table

---
+++ src/db/models/orders/orders_items.py
@@ -0,0 +1,21 @@
+from uuid import UUID
+from decimal import Decimal
+from sqlalchemy import ForeignKey, Numeric
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy.dialects.postgresql import UUID
+
+from db.base import Base
+from models.types import uuid_pk
+
+
+
+class OrderItem(Base):
+    __tablename__ = "order_items"
+
+    id: Mapped[uuid_pk]
+    order_id: Mapped[UUID] = mapped_column(ForeignKey("orders.id"), nullable=False)
+    product_id: Mapped[UUID] = mapped_column(ForeignKey("products.id"), nullable=False)
+    quantity: Mapped[int] = mapped_column(nullable=False)
+    price: Mapped[Decimal] = mapped_column(Numeric(10, 2), nullable=False)
+
+    order: Mapped["Order"] = relationship("Order", back_populates="items")
