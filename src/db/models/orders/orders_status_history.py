238faca3b28d15f54966ad66921d28fd904e5fc7
Misha Kutsov <misha@misha.ru>
2025-08-31 00:36:31 +0500

    add orders status history table

---
+++ src/db/models/orders/orders_status_history.py
@@ -0,0 +1,19 @@
+from uuid import UUID
+from datetime import datetime
+from sqlalchemy import ForeignKey, Enum, DateTime
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from db.base import Base
+from models.types import uuid_pk
+from models.enums import OrderStatusEnum
+
+
+class OrderStatusHistory(Base):
+    __tablename__ = "order_status_history"
+
+    id: Mapped[uuid_pk]
+    order_id: Mapped[UUID] = mapped_column(ForeignKey("orders.id"), nullable=False)
+    status: Mapped[OrderStatusEnum] = mapped_column(Enum(OrderStatusEnum), nullable=False)
+    changed_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.utcnow, nullable=False)
+
+    order: Mapped["Order"] = relationship("Order", back_populates="status_history")
