002685e28b571c476ce0485546335a6ac9cdd022
Your Name <you@example.com>
2025-09-01 01:13:50 +0500

    add transactions table

---
+++ src/db/models/finances/transactions.py
@@ -0,0 +1,21 @@
+from decimal import Decimal
+from uuid import UUID
+
+from sqlalchemy import Numeric, ForeignKey, Enum
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from models.types import uuid_pk, created_at
+from models.enums import TransactionTypeEnum
+from db.base import Base
+
+
+class Transaction(Base):
+    __tablename__ = "transactions"
+
+    id: Mapped[uuid_pk]
+    wallet_id: Mapped[UUID] = mapped_column(ForeignKey("wallets.id"), nullable=False)
+    type: Mapped[TransactionTypeEnum] = mapped_column(Enum(TransactionTypeEnum), nullable=False)
+    amount: Mapped[Decimal] = mapped_column(Numeric(12, 2), nullable=False)
+    created_at: Mapped[created_at]
+
+    wallet: Mapped["Wallet"] = relationship("Wallet", back_populates="transactions")
