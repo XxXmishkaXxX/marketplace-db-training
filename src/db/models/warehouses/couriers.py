2438db52b22ba862db879cfb7f8206ac2a016609
Your Name <you@example.com>
2025-09-01 00:37:21 +0500

    add couriers table

---
+++ src/db/models/warehouses/couriers.py
@@ -0,0 +1,20 @@
+from uuid import UUID
+from sqlalchemy import ForeignKey, Float
+from sqlalchemy.orm import relationship, Mapped, mapped_column
+
+
+from db.base import Base
+from models.types import uuid_pk, str256_not_null
+
+
+class Courier(Base):
+    __tablename__ = "couriers"
+
+    id: Mapped[uuid_pk]
+    user_id: Mapped[UUID] = mapped_column(ForeignKey("users.id"), nullable=False)
+    name: Mapped[str256_not_null]
+    transport: Mapped[str256_not_null]
+    rating: Mapped[float] = mapped_column(Float, nullable=True)
+
+    user: Mapped["User"] = relationship(back_populates="courier_profile")
+    assignments: Mapped[list["CourierAssignment"]] = relationship(back_populates="courier")
