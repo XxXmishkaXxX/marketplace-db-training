a04b56e0a3932f979e0b1431c6acb809398487fd
Misha Kutsov <misha@misha.ru>
2025-09-05 23:40:52 +0500

    add sync_engine and sync_session_maker

--- src/db/base.py
+++ src/db/base.py
@@ -1,17 +1,21 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.ext.asyncio import create_async_engine
+from sqlalchemy import create_engine
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
 
 
 from db.config_db import settings
 
 
-engine = create_async_engine(settings.DATABASE_URL, echo=True)
+engine = create_async_engine(settings.DATABASE_URL_ASYNC, echo=True)
 async_session = sessionmaker(
     engine, class_=AsyncSession, expire_on_commit=False
 )
 
-
+sync_engine = create_engine(settings.DATABASE_URL_SYNC)
+sync_session = sessionmaker(
+    sync_engine, expire_on_commit=False
+)
 
 class Base(DeclarativeBase):
     type_annotation_map = {}
